d7ecb2ae8d8b399c7867df2b2b472d2afa016bdb
godofevil <rkolodiazhniy@odessa.gov.ua>
2020-05-28 14:02:34 +0300

    upload

---
+++ precache-manifest.e1ac08663c5b35d126980979a39706e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bc3ada5717176fbb1044a4fb1b2f336",
+    "url": "/boilerplate/index.html"
+  },
+  {
+    "revision": "606bf587668d9255dcd4",
+    "url": "/boilerplate/static/css/main.da7c3b00.chunk.css"
+  },
+  {
+    "revision": "9a96b04bf9b4e60e27fb",
+    "url": "/boilerplate/static/js/2.3b007176.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/boilerplate/static/js/2.3b007176.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bac88790447d4eebd343",
+    "url": "/boilerplate/static/js/3.26d4d6b1.chunk.js"
+  },
+  {
+    "revision": "606bf587668d9255dcd4",
+    "url": "/boilerplate/static/js/main.77622c0c.chunk.js"
+  },
+  {
+    "revision": "4d0f235d06fbb6a3696c",
+    "url": "/boilerplate/static/js/runtime-main.2e2e281a.js"
+  }
+]);
